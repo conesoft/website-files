fe3b3b1110435f6dfff077229b4f8dd27ca6f0a8
David Spörri <davepermen@hotmail.com>
2023-09-03 13:56:25 +0200

    storagepool added

---
+++ src/Services/StoragePool.cs
@@ -0,0 +1,22 @@
+﻿using System.Management;
+
+record StoragePool(string FriendlyName, ulong Size, ulong AllocatedSize)
+{
+    private static IEnumerable<ManagementObject> Query =>
+        new ManagementObjectSearcher(
+            scope: "root\\Microsoft\\Windows\\Storage",
+            queryString: "SELECT * FROM MSFT_StoragePool WHERE IsPrimordial=False"
+        )
+        .Get()
+        .Cast<ManagementObject>();
+
+    public static StoragePool? Current => Query.FirstOrDefault() switch
+    {
+        ManagementObject pool => new StoragePool(
+            FriendlyName:  (string) pool.GetPropertyValue(nameof(FriendlyName)),
+            Size:          (ulong)  pool.GetPropertyValue(nameof(Size)),
+            AllocatedSize: (ulong)  pool.GetPropertyValue(nameof(AllocatedSize))
+        ),
+        null => null
+    };
+};
