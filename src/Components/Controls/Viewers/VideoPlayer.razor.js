5ba92c25d3fd798a5ef0bf9701a969f23cbbec2b
David Spörri <davepermen@hotmail.com>
2024-03-19 18:58:46 +0100

    fixes n features

--- src/Components/Controls/Viewers/VideoPlayer.razor.js
+++ src/Components/Controls/Viewers/VideoPlayer.razor.js
@@ -1,3 +1,25 @@
+export function registerGlobalKeyboardEvents(assembly, methodName) {
+    let serializeEvent = function (e) {
+        if (e) {
+            return {
+                key: e.key,
+                code: e.keyCode.toString(),
+                location: e.location,
+                repeat: e.repeat,
+                ctrlKey: e.ctrlKey,
+                shiftKey: e.shiftKey,
+                altKey: e.altKey,
+                metaKey: e.metaKey,
+                type: e.type
+            };
+        }
+    };
+    
+    window.document.addEventListener('keydown', function (e) {
+        DotNet.invokeMethodAsync(assembly, methodName, serializeEvent(e))
+    });
+}
+
 export function updateMediaSession(title, artist, image) {
     if ("mediaSession" in navigator) {
         navigator.mediaSession.metadata = new MediaMetadata({
