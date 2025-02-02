9db1b4fb412d054d6d62f5f0980cd7eec4f47da2
David Spörri <davepermen@hotmail.com>
2025-02-02 23:00:11 +0100

    let's go

---
+++ src/Components/EntryCategories/Logs/Category.cs
@@ -0,0 +1,12 @@
+﻿using Conesoft.Files;
+using System.Collections;
+
+namespace Conesoft.Website.Files.Components.EntryCategories.Directories
+{
+    public class Category
+    {
+        public void From(IEnumerable<Conesoft.Files.Entry> entries)
+        {
+        }
+    }
+}
